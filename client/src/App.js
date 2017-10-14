21ed324b85182fcb15ebe2b9ea67269ad875a56c
Kevin Hughes <kevinhughes27@gmail.com>
2017-10-14 10:59:00 -0400

    connect truffle and web3 to create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import EventContract from './contracts/Event.json'
+import getWeb3 from './utils/getWeb3'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      web3: null,
+      array: []
+    }
+  }
+
+  componentWillMount() {
+    getWeb3
+    .then(results => {
+      this.setState({ web3: results.web3 })
+      this.instantiateContract()
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract() {
+    const contract = require('truffle-contract')
+    const eventContract = contract(EventContract)
+    eventContract.setProvider(this.state.web3.currentProvider)
+
+    eventContract.deployed().then((instance) => {
+      instance.getArray.call().then((array) => {
+        return this.setState({array: array})
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.array.toString()}
       </div>
     );
   }
--- contracts/Event.sol
+++ contracts/Event.sol
@@ -6,4 +6,8 @@ contract Event {
   function Event(int16[10] _initArray) {
     testArray = _initArray;
   }
+
+  function getArray() public returns (int16[10]) {
+    return testArray;
+  }
 }
