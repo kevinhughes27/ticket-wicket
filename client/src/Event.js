abded01f6c76cedfb9047f035e280b28cff67c9f
Kevin Hughes <kevinhughes27@gmail.com>
2017-10-14 14:46:15 -0400

    add react-router and new components for creating a new event and viewing an exiting event

---
+++ client/src/Event.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import EventContract from './contracts/Event.json'
+import getWeb3 from './utils/getWeb3'
+
+class Event extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      web3: null,
+      tickets: []
+    }
+  }
+
+  componentWillMount() {
+    getWeb3
+    .then(results => {
+      this.setState({ web3: results.web3 })
+      this.instantiateContract()
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract() {
+    const contract = require('truffle-contract')
+    const eventContract = contract(EventContract)
+    const contractAddress = this.props.match.params.contractAddress
+
+    eventContract.setProvider(this.state.web3.currentProvider)
+
+    eventContract.at(contractAddress).then((instance) => {
+      instance.getTickets.call().then((data) => {
+        let tickets = []
+        let numTickets = data[0].length
+        for (let i = 0; i < numTickets; i++) {
+          tickets.push({
+            identifier: data[0][i].toNumber(),
+            price: data[1][i].toNumber()
+          })
+        }
+        return this.setState({tickets: tickets})
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.tickets.map(this.renderTicket)}
+        </ul>
+      </div>
+    );
+  }
+
+  renderTicket(ticket) {
+    return (
+      <li key={ticket.identifier}>
+        {ticket.identifier} {ticket.price}
+      </li>
+    )
+  }
+}
+
+Event.propTypes = {
+  contractAddress: PropTypes.string
+};
+
+export default Event;
---
+++ client/src/NewEvent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewEvent extends Component {
+  render () {
+    return (
+      <div>
+        New Event
+      </div>
+    )
+  }
+}
+
+export default NewEvent
