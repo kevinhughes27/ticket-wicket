7bea96ccab49da9b9dc97b60331bc132d36d088a
Ben Cox <ben@shopify.com>
2017-10-14 22:01:40 -0400

    Style the vierfier

--- client/src/components/Verifier.js
+++ client/src/components/Verifier.js
@@ -3,6 +3,7 @@ import EventContract from '../contracts/Event.json'
 import QrReader from 'react-qr-reader'
 import { CircularProgress } from 'react-md'
 import { sha3_256 } from 'js-sha3'
+import { Grid, Cell, Card, CardTitle, MediaOverlay, Media, CardText, Button} from 'react-md'
 
 class Verifier extends Component {
   constructor(props) {
@@ -63,20 +64,31 @@ class Verifier extends Component {
       return <CircularProgress id="loader" />
     } else if (this.state.scan == null) {
       const preview = {
-        width: 100,
-        height: 100
+        width: '100%',
+        display: 'inline'
       }
       return (
-          <div>
-            <QrReader
-              delay={100}
-              previewStyle={preview}
-              onError={this.handleError}
-              onScan={this.handleScan}
-            />
-          </div>
+        <Grid>
+          <Cell size={8} offset={2}>
+            <Card>
+              <Media>
+                <QrReader
+                  delay={100}
+                  style={preview}
+                  onError={this.handleError}
+                  onScan={this.handleScan}
+                />
+                <MediaOverlay>
+                  <CardTitle title="Verify ticket" subtitle="Place your ticket's QR code in front of your camera">
+                  </CardTitle>
+                </MediaOverlay>
+              </Media>
+            </Card>
+          </Cell>
+        </Grid>
           )
     } else {
+
       let valid = (
           <div>
             Verifying:
@@ -84,17 +96,37 @@ class Verifier extends Component {
           </div>
           )
       if (this.state.verified === true) {
-        valid = <p> True </p>
-      } else if (this.state.verified === false) {
-        valid = <p> False </p>
-      }
-      return (
-        <div>
-          <p> Scanned: {this.state.scan.name} </p>
-          {valid}
-          <button onClick={this.reset.bind(this)}> Next Ticket </button>
-        </div>
+        return (
+          <Grid>
+            <Cell size={4} offset={4}>
+              <Card>
+                <CardTitle style={{background:'yellowgreen'}} title='Ticket Verified!' />
+                <CardText>
+                  <h3>We successfully validated this ticket</h3>
+                  <h4>Name on the Ticket: {this.state.scan.name}</h4>
+                </CardText>
+                <Button raised primary onClick={this.reset.bind(this)}> Next Ticket</Button>
+              </Card>
+            </Cell>
+          </Grid>
         )
+      } else if (this.state.verified === false) {
+        return (
+          <Grid>
+            <Cell size={4} offset={4}>
+              <Card>
+                <CardTitle style={{background:'orangered'}} title='Ticket Refuted!' />
+                <CardText>
+                  <h3>We could not verify this ticket</h3>
+                  <h4>Name on the Ticket: {this.state.scan.name}</h4>
+                </CardText>
+                <Button raised primary onClick={this.reset.bind(this)}> Next Ticket</Button>
+              </Card>
+            </Cell>
+          </Grid>
+        )
+      }
+      return ('')
     }
   }
 }
